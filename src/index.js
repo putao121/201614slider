69c63685f4ab7cb7cf009f8d335515bad35a28ac
zhufengnodejs <zhufengnodejs@126.com>
2017-03-28 11:21:57 +0800

    1

--- src/index.js
+++ src/index.js
@@ -8,4 +8,9 @@ const images = [
     {src:require('./images/4.jpg'),alt:'4'}
 ]
 
-render(<Slider images={images}/>,document.querySelector('#app'))
+render(<Slider
+    images={images}
+    autoPlay={true}
+    speed={1}
+    delay={2}
+/>,document.querySelector('#app'))
